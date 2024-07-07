8ee5a569cfd1ee058fdbe5a39fb153a9356c90bf
suhdonggeun <99003874+donggeun-suh@users.noreply.github.com>
2024-07-08 00:13:45 +0900

    Time: 23 ms (8.37%) | Memory: 49.6 MB (72.92%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        Integer count = null;
+        int majority = 1 +  nums.length / 2; 
+        
+        for(int i  = 0; i < nums.length; i++) {
+           count = countMap.get(nums[i]);
+            
+            if(count == null){
+                countMap.put(nums[i], 1);
+            } else {
+                countMap.put(nums[i], count + 1);
+            }
+            
+            if (countMap.get(nums[i]) >= majority){
+                    return nums[i];
+            }
+        }
+        
+        return -1;
+    }
+}
